d98cf4214f954d403587dcaad8b1baaac5f747ab
Marcel Boussard <Tresoramoebe688@gmail.com>
2022-10-13 10:39:57 +0200

    add files

---
+++ 8kyu/logicDrills_trafficLights.js
@@ -0,0 +1,12 @@
+function updateLight(current) {
+    // no variables needed
+    // green, yellow, red, green
+    //returns updated lights
+    // turnary operators to update light color
+    
+  
+    return current === 'yellow' ? 'red' : current === 'green' ? 'yellow' : 'green';
+    
+    
+  
+  }
