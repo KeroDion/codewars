88af76b12af0b1f1824e70bb2d6f76eb0217af82
Marcel Boussard <Tresoramoebe688@gmail.com>
2022-11-14 12:34:13 +0100

    return the highest scoring word a = 1, b = 2 points ...

---
+++ 6kyu/highestScoringWord.js
@@ -0,0 +1,24 @@
+function high(x){
+    // splitting string into an array of single words
+    const words = x.split(' ');
+    // creating an array with the length of words array, filled with 0 from the start
+    const num = Array(words.length).fill(0);
+    //iterate through words array to calculate score of single word
+    for (let i = 0; i < words.length; ++i) {
+      //binding single word to a new variable, now we can work with it without chaning original array
+      const word = words[i];
+      //Work through the letters of the single word and sum up the score into the num[i] place of the num array
+      for (let j = 0; j < word.length; ++j) {
+        num[i] += word.charCodeAt(j) - 96;
+      }
+    }
+    //get index of highest score from the num array
+    let highestScore = num.indexOf(Math.max(...num))
+    //returning the word with the highest score, using the index of num array. 
+    //The index with the highest score in num is the same index with the highest word in words array
+    return words[highestScore]
+  }
+  
+    
+  
+  
