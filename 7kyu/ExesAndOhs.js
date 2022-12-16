d513dab68e3dad1511ec75a893c1c652863fe081
Marcel Boussard <Tresoramoebe688@gmail.com>
2022-12-16 12:20:14 +0100

    Now using str.match and comparing arrays and their lengthes

--- 7kyu/ExesAndOhs.js
+++ 7kyu/ExesAndOhs.js
@@ -1,15 +1,21 @@
-function XO(str) {
-    //code here
-  let countX = 0
-  let countO = 0
-  let word = str.toLowerCase()
-  for(let i = 0; i < word.length; i++){
+// function XO(str) {
+//     //code here
+//   let countX = 0
+//   let countO = 0
+//   let word = str.toLowerCase()
+//   for(let i = 0; i < word.length; i++){
     
-    if(word[i] === "x"){
-      countX++
-    }else if(word[i] == "o"){
-      countO++
-    }
-  }
-  return countX === countO
+//     if(word[i] === "x"){
+//       countX++
+//     }else if(word[i] == "o"){
+//       countO++
+//     }
+//   }
+//   return countX === countO
+// }
+
+function XO(str) {
+  let x = str.match(/x/gi); // returns an Array
+  let o = str.match(/o/gi);
+  return (x && x.length) === (o && o.length);
 }
