75cf4f45fedf32aec441a5f1830486ed8c3dddcb
Marcel Boussard <Tresoramoebe688@gmail.com>
2022-12-17 20:21:15 +0100

    calculating the Integer number now with parseInt

--- 7kyu/onesAndZeros.js
+++ 7kyu/onesAndZeros.js
@@ -1,8 +1,1 @@
-const binaryArrayToNumber = arr => {
-  let sumInt = 0
-  for(let i = 0; i < arr.length; i++){
-    arr[i] === 0 ? sumInt += 0 : sumInt += 2 ** (arr.length - 1 - i)
-    
-  }
-  return sumInt
-};
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2);
